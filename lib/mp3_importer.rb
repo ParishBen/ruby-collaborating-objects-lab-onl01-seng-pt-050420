5b276754295615fcc7db3fc0c29b736b65dea7d8
Ben Parish <parishben13@gmail.com>
2020-05-31 21:41:01 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,6 +1,6 @@
 class MP3Importer
   attr_accessor :music_importer
-  def initialize(music_importer)
-    @music_importer=music_importer
+  def initialize(test_path)
+    @music_importer=test_path
   end
 end 
